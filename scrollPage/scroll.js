5d756a0c1cb6dde39cf6d528dbb5b8cd85514b17
oozoosun <kamilla7@gmail.com>
2023-10-13 10:46:53 +0900

    scroll.2ver
    scroll.2ver

--- scrollPage/scroll.js
+++
@@ -1,8 +0,0 @@
-const elemSec1 = document.querySelector('.sec1');
-const onEventSection1 = (event) => {
-    event.preventDefault();
-    if( event.deltaY > 0 ) {
-        elemSec2.style.top = '0';
-    }
-}
-elemSec1.addEventListener('wheel',onEventSection1,{passive:false});
